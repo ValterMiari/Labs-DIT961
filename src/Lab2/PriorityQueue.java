3addca8f5f0d4984a2e68562ecadf223daf5ba6f
GabbeHags <38500419+GabbeHags@users.noreply.github.com>
2021-04-15 11:56:06 +0200

    extended bench

--- src/Lab2/PriorityQueue.java
+++ src/Lab2/PriorityQueue.java
@@ -4,98 +4,98 @@ import java.util.*;
 
 // A priority queue.
 public class PriorityQueue<E> {
-	private ArrayList<E> heap = new ArrayList<E>();
-	private Comparator<E> comparator;
+    private ArrayList<E> heap = new ArrayList<E>();
+    private Comparator<E> comparator;
 
-	public PriorityQueue(Comparator<E> comparator) {
-		this.comparator = comparator;
-	}
+    public PriorityQueue(Comparator<E> comparator) {
+        this.comparator = comparator;
+    }
 
-	// Returns the size of the priority queue.
-	public int size() {
-		return heap.size();
-	}
-     
-	// Adds an item to the priority queue.
-	public void add(E x)
-	{
-		throw new UnsupportedOperationException();
-	}
+    // Returns the size of the priority queue.
+    public int size() {
+        return heap.size();
+    }
 
-	// Returns the smallest item in the priority queue.
-	// Throws NoSuchElementException if empty.
-	public E minimum() {
-		if (size() == 0)
-			throw new NoSuchElementException();
+    // Adds an item to the priority queue.
+    public void add(E x) {
+        System.out.println("added: " + x.toString() + " to " + comparator.getClass().getSimpleName() + " priority que" );
+//        throw new UnsupportedOperationException();
+    }
 
-		return heap.get(0);
-	}
+    // Returns the smallest item in the priority queue.
+    // Throws NoSuchElementException if empty.
+    public E minimum() {
+        if (size() == 0)
+            throw new NoSuchElementException();
 
-	// Removes the smallest item in the priority queue.
-	// Throws NoSuchElementException if empty.
-	public void deleteMinimum() {
-		if (size() == 0)
-			throw new NoSuchElementException();
+        return heap.get(0);
+    }
 
-		heap.set(0, heap.get(heap.size()-1));
-		heap.remove(heap.size()-1);
-		if (heap.size() > 0) siftDown(0);
-	}
+    // Removes the smallest item in the priority queue.
+    // Throws NoSuchElementException if empty.
+    public void deleteMinimum() {
+        if (size() == 0)
+            throw new NoSuchElementException();
 
-	// Sifts a node up.
-	// siftUp(index) fixes the invariant if the element at 'index' may
-	// be less than its parent, but all other elements are correct.
-	private void siftUp(int index) {
-		throw new UnsupportedOperationException();
-	}
-     
-	// Sifts a node down.
-	// siftDown(index) fixes the invariant if the element at 'index' may
-	// be greater than its children, but all other elements are correct.
-	private void siftDown(int index) {
-		E value = heap.get(index);
-		
-		// Stop when the node is a leaf.
-		while (leftChild(index) < heap.size()) {
-			int left    = leftChild(index);
-			int right   = rightChild(index);
+        heap.set(0, heap.get(heap.size() - 1));
+        heap.remove(heap.size() - 1);
+        if (heap.size() > 0) siftDown(0);
+    }
 
-			// Work out whether the left or right child is smaller.
-			// Start out by assuming the left child is smaller...
-			int child = left;
-			E childValue = heap.get(left);
+    // Sifts a node up.
+    // siftUp(index) fixes the invariant if the element at 'index' may
+    // be less than its parent, but all other elements are correct.
+    private void siftUp(int index) {
+        throw new UnsupportedOperationException();
+    }
 
-			// ...but then check in case the right child is smaller.
-			// (We do it like this because maybe there's no right child.)
-			if (right < heap.size()) {
-				E rightValue = heap.get(right);
-				if (comparator.compare(childValue, rightValue) > 0) {
-					child = right;
-					childValue = rightValue;
-				}
-			}
+    // Sifts a node down.
+    // siftDown(index) fixes the invariant if the element at 'index' may
+    // be greater than its children, but all other elements are correct.
+    private void siftDown(int index) {
+        E value = heap.get(index);
 
-			// If the child is smaller than the parent,
-			// carry on downwards.
-			if (comparator.compare(value, childValue) > 0) {
-				heap.set(index, childValue);
-				index = child;
-			} else break;
-		}
+        // Stop when the node is a leaf.
+        while (leftChild(index) < heap.size()) {
+            int left = leftChild(index);
+            int right = rightChild(index);
 
-		heap.set(index, value);
-	}
+            // Work out whether the left or right child is smaller.
+            // Start out by assuming the left child is smaller...
+            int child = left;
+            E childValue = heap.get(left);
 
-	// Helper functions for calculating the children and parent of an index.
-	private final int leftChild(int index) {
-		return 2*index+1;
-	}
+            // ...but then check in case the right child is smaller.
+            // (We do it like this because maybe there's no right child.)
+            if (right < heap.size()) {
+                E rightValue = heap.get(right);
+                if (comparator.compare(childValue, rightValue) > 0) {
+                    child = right;
+                    childValue = rightValue;
+                }
+            }
 
-	private final int rightChild(int index) {
-		return 2*index+2;
-	}
+            // If the child is smaller than the parent,
+            // carry on downwards.
+            if (comparator.compare(value, childValue) > 0) {
+                heap.set(index, childValue);
+                index = child;
+            } else break;
+        }
 
-	private final int parent(int index) {
-		return (index-1)/2;
-	}
+        heap.set(index, value);
+    }
+
+    // Helper functions for calculating the children and parent of an index.
+    private final int leftChild(int index) {
+        return 2 * index + 1;
+    }
+
+    private final int rightChild(int index) {
+        return 2 * index + 2;
+    }
+
+    private final int parent(int index) {
+        return (index - 1) / 2;
+    }
 }
